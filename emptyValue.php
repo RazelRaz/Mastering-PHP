5ef524377a7e87be2fb44a99ba7661da632e7b64
RazelRaz <razel16dcm@gmail.com>
2024-03-18 13:05:55 +0600

    Empty value

---
+++ emptyValue.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = " ";
+
+if (isset($name)) {
+  echo "Name is Set";
+} else {
+  echo "Not SET";
+}
+
+echo "<br>";
+
+if(empty($name)){
+  echo "NAME IS EMPTY";
+} else {
+  echo "Not Empty";
+}
