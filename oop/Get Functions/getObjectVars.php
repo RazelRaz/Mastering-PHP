2a3f5c8646184c94328cf14cae97b2640e9287ad
RazelRaz <razel16dcm@gmail.com>
2024-06-19 15:51:50 +0600

    get_object_vars

--- oop/Get Functions/getObjectVars.php
+++ oop/Get Functions/getObjectVars.php
@@ -1,5 +1,6 @@
 <?php
-//get_class_vars()
+//get_object_vars()
+
 
 class gtcvMyclass {
     public $myvar;
@@ -9,12 +10,16 @@ class gtcvMyclass {
 
     public function __construct()
     {
-        print_r(get_class_vars(__CLASS__));
+        $this->myvar = "R";
+        $this->secondVar = "A";
+        echo "<pre>";
+        print_r(get_object_vars($this));
+        echo "</pre>";
     }
 }
 
 $gtcObj = new gtcvMyclass();
 
 // gget_class_vars will return a assciative arry & get_class will catch the object
-$gtcVar = get_class_vars(get_class($gtcObj));
+$gtcVar = get_object_vars($gtcObj);
 print_r($gtcVar);
