22af039da6a34867baf03d21ba9ad71d193dabf3
RazelRaz <razel16dcm@gmail.com>
2024-06-19 14:32:11 +0600

    get_class_methods

--- oop/Get Functions/getClassMethods.php
+++ oop/Get Functions/getClassMethods.php
@@ -1,9 +1,33 @@
 <?php
 // get_class_methods()
 // Gets the class methods' names
+// get_class_methods will not return the private or protected methods
+//But if we call get_class_methods inside its class than it will return all methods including private and protected
 
 class cmnMyClass {
-    
+    public function __construct()
+    {
+        print_r(get_class_methods($this));
+    }
+    public function myfunc1(){
+
+    }
+    private function myfunc2(){
+        
+    }
+    public function muFunc3(){
+
+    }
+
 }
 
 $cmnObj = new cmnMyClass();
+
+$gtCls = get_class_methods('cmnMyClass');
+// get_class_methods returning an array thats why we used print_r below
+print_r($gtCls);
+
+foreach ($gtCls as $methods) {
+    echo $methods . "<br>";
+}
+
